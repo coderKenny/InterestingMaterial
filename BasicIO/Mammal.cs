524ba468e220552579f5db8aeb72782d38ff8497
Kenneth Ekbom <kenne@dnainternet.net>
2014-06-11 21:41:27 +0300

    Second Push

---
+++ BasicIO/Mammal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task17
+{
+    internal class Mammal:MyDiskIOOperations
+    {
+        int ika;
+        string vari, nimi;
+
+        public Mammal(string nimi)
+        {
+            this.nimi = nimi;
+        }
+
+        internal virtual void kavelee()
+        {
+            Console.WriteLine(this.nimi + " niminen nisäkäs kävelee");
+
+        }
+
+        internal virtual void puhuu()
+        {
+            Console.WriteLine(this.nimi + " niminen nisäkäs puhuu");
+        }
+
+        internal virtual int kertoo_ikansa()
+        {
+            return this.ika;
+        }
+
+        internal virtual void asettaa_ikansa(int ika)
+        {
+            this.ika = ika;
+        }
+
+
+        public void MySaveToDisk()
+        {
+            // WriteAllText
+            File.WriteAllText("mammal.txt",this.GetType().ToString());
+            Console.WriteLine(this.GetType().ToString());
+            Console.WriteLine("Data written");
+            Apuluokka.InsertWideLineSepatator();
+ 
+        }
+
+        public string MyReadFromDisk()
+        {
+            // Read in every line in specified file.
+            // ... This will store all lines in an array in memory.
+            string[] lines = File.ReadAllLines("mammal.txt");
+
+            Console.WriteLine(lines[0]);
+        
+            Console.WriteLine("Data read");
+            Apuluokka.InsertWideLineSepatator();
+
+            return lines[0];
+        
+        }
+
+
+    }
+}
---
+++ BasicIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valinta;
+            string rivi="";
+            Mammal nisakas = new Mammal("Nisuli");
+            
+
+
+            nisakas.puhuu();
+
+            Console.WriteLine("Talleta data anna 1, muu numero lue data");
+            valinta = int.Parse(Console.ReadLine());
+
+            if (valinta == 1)
+                nisakas.MySaveToDisk();
+
+            else
+                rivi=nisakas.MyReadFromDisk();
+
+           
+            if(valinta!=1)
+                Console.WriteLine("Luettu data uudestaan "+rivi+" pääohjelmasta !");
+
+            Apuluokka.Pause();
+        }
+    }
+}
